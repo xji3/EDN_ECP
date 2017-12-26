c869467342c4e36912f9c3d04e9e216e899e5f5b
xji3 <xji3@ncsu.edu>
2017-12-26 00:54:01 -0500

    update summary

--- Run_Force.py
+++ Run_Force.py
@@ -74,6 +74,36 @@ if __name__ == '__main__':
     IGC_pm = 'One rate'
     seq_index_file = './' + '_'.join(paralog) +'_seq_index.txt'
 
+    force = None
+    if args.rate_variation:
+        save_file = './save/JS_HKY_'+ '_'.join(paralog) + '_' + IGC_pm.replace(' ', '_') + '_rv_nonclock_save.txt'
+        log_file = './log/JS_HKY_'+ '_'.join(paralog) + '_' + IGC_pm.replace(' ', '_') + '_rv_nonclock_log.txt'
+        summary_file = './summary/JS_HKY_'+ '_'.join(paralog) + '_' + IGC_pm.replace(' ', '_') + '_rv_nonclock_summary.txt'
+        gradient_file = './summary/JS_HKY_'+ '_'.join(paralog) + '_' + IGC_pm.replace(' ', '_') + '_rv_nonclock_gradient.txt'
+        hessian_file = './summary/JS_HKY_'+ '_'.join(paralog) + '_' + IGC_pm.replace(' ', '_') + '_rv_nonclock_hessian.txt'
+        godambe_file = './summary/JS_HKY_'+ '_'.join(paralog) + '_' + IGC_pm.replace(' ', '_') + '_rv_nonclock_godambe.txt'
+        x_js = np.log([ 0.5, 0.5, 0.5,  4.35588244, 0.5, 5.0, 0.3])
+    else:
+        save_file = './save/JS_HKY_'+ '_'.join(paralog) + '_' + IGC_pm.replace(' ', '_') + '_nonclock_save.txt'
+        log_file = './log/JS_HKY_'+ '_'.join(paralog) + '_' + IGC_pm.replace(' ', '_') + '_nonclock_log.txt'
+        summary_file = './summary/JS_HKY_'+ '_'.join(paralog) + '_' + IGC_pm.replace(' ', '_') + '_nonclock_summary.txt'
+        gradient_file = './summary/JS_HKY_'+ '_'.join(paralog) + '_' + IGC_pm.replace(' ', '_') + '_nonclock_gradient.txt'
+        hessian_file = './summary/JS_HKY_'+ '_'.join(paralog) + '_' + IGC_pm.replace(' ', '_') + '_nonclock_hessian.txt'
+        godambe_file = './summary/JS_HKY_'+ '_'.join(paralog) + '_' + IGC_pm.replace(' ', '_') + '_nonclock_godambe.txt'
+        x_js = np.log([ 0.5, 0.5, 0.5,  4.35588244,   0.3])
+
+
+
+    test_JS = JSGeneconv(alignment_file, gene_to_orlg_file, args.cdna, tree_newick, DupLosList, x_js, pm_model, IGC_pm,
+                         args.rate_variation, node_to_pos, terminal_node_list, save_file, force = force)
+    test_JS.get_mle()
+    #test_JS.get_expectedNumGeneconv()
+    test_JS.get_individual_summary(summary_file)
+    godambe = test_JS.get_Godambe_matrix(test_JS.x, gradient_file, hessian_file, 1e-6)
+    np.savetxt(open(godambe_file, 'w+'), np.array(godambe))
+
+
+
     force = {6:0.0}
     if args.rate_variation:
         save_file = './save/Force_JS_HKY_'+ '_'.join(paralog) + '_' + IGC_pm.replace(' ', '_') + '_rv_nonclock_save.txt'
@@ -88,9 +118,9 @@ if __name__ == '__main__':
 
 
 
-    test_JS = JSGeneconv(alignment_file, gene_to_orlg_file, args.cdna, tree_newick, DupLosList, x_js, pm_model, IGC_pm,
+    test_JS_Force = JSGeneconv(alignment_file, gene_to_orlg_file, args.cdna, tree_newick, DupLosList, x_js, pm_model, IGC_pm,
                          args.rate_variation, node_to_pos, terminal_node_list, save_file, force = force)
-    test_JS.get_mle()
+    test_JS_Force.get_mle()
     #test_JS.get_expectedNumGeneconv()
-    test_JS.get_individual_summary(summary_file)
+    test_JS_Force.get_individual_summary(summary_file)
         
